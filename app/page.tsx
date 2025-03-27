56f1161d47b52d4ab64eba080df10382dce5fd48
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2025-03-27 14:05:45 -0300

    feat: added hero section on home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import HeroSection from "./components/hero-section";
 import NavigationSection from "./components/navigation-section";
 
 export default function Home(){
   return(
     <div className="min-h-screen bg-background-primary">
       <NavigationSection/>
-      
+      <HeroSection/>
     </div>
   )
 }
