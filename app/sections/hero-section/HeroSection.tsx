39ebf3f0e0ace8f5eab24dec03f0144b4d8d9544
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2025-03-27 17:56:23 -0300

    fix: changed <h1> content

--- app/sections/hero-section/HeroSection.tsx
+++ app/sections/hero-section/HeroSection.tsx
@@ -2,11 +2,10 @@ import Image from "next/image"
 
 export default function HeroSection(){
     return(
-        <div className="border-b ">
             <section className="container  mx-auto md:grid md:grid-cols-2 gap-4 md:justify-items-center my-22 flex flex-col items-center" >
                 <div className="flex flex-col max-md:items-center min-md:gap-y-2 max-md:space-y-2">
                     <h1 className="md:text-7xl text-4xl font-bold from-neutral-900">
-                        Software Engineer
+                        Software Developer
                     </h1>
                     <span className="max-md:mx-2 max-md:text-center text-bg-primary-active md:text-2xl text-lg ">I build accessible, user-friendly web applications with modern technologies.</span>
                     <div className="space-x-2 max-md:flex max-md:flex-col max-md:space-y-2 ">
@@ -27,7 +26,5 @@ export default function HeroSection(){
                     />            
                 </div>
             </section>
-
-        </div>
     )
 }
